4fe7b85feae1d7dc831ffe963f22e39d5a371f1d
VIshal6620 <vishalvish7099@gmail.com>
2025-02-13 15:26:14 +0530

    done

---
+++ sop_django/ORS/ctl/FavoriteCtl.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from ORS.ctl.BaseCtl import BaseCtl
+from ORS.utility.DataValidator import DataValidator
+from service.models import Favorite
+from service.service.FavoriteService import FavoriteService
+
+
+class FavoriteCtl(BaseCtl):
+
+    def request_to_form(self, requestForm):
+        self.form['id'] = requestForm['id']
+        self.form['product'] = requestForm['product']
+        self.form['addedDate'] = requestForm['addedDate']
+
+    def form_to_model(self, obj):
+        pk = int(self.form['id'])
+        if pk > 0:
+            obj.id = pk
+            obj.product = self.form['product']
+            obj.addedDate = self.form['addedDate']
+        return obj
+
+    def model_to_form(self, obj):
+        if obj is None:
+            return obj
+        self.form['id'] = obj.id
+        self.form['product'] = obj.product
+        self.form['addedDate'] = obj.addedDate
+
+    def input_validation(self):
+        super().input_validation()
+        inputError = self.form['inputError']
+
+        if (DataValidator.isNull(self.form['product'])):
+            inputError['product'] = "product is Required"
+            self.form['error'] = True
+        else:
+            if (DataValidator.isalphacehck(self.form['product'])):
+                inputError['product'] = "product is contain"
+                self.form['error'] = True
+
+        if (DataValidator.isNull(self.form['addedDate'])):
+            inputError['addedDate'] = "addedDate is Required"
+            self.form['error'] = True
+        else:
+            if (DataValidator.isDate(self.form['addedDate'])):
+                inputError['addedDate'] = "addedDate is YYYY-MM-DD Contain"
+                self.form['error'] = True
+
+        return self.form['error']
+
+    def display(self, request, params={}):
+        if (params['id'] > 0):
+            obj = self.get_service().get(params['id'])
+            self.model_to_form(obj)
+        res = render(request, self.get_template(), {'form': self.form})
+        return res
+
+    def submit(self, request, params={}):
+        r = self.form_to_model(Favorite())
+        self.get_service().save(r)
+        self.form['message'] = "Data saved successfully"
+        res = render(request, self.get_template(), {'form': self.form})
+        return res
+
+    def get_template(self):
+        return "Favorite.html"
+
+    def get_service(self):
+        return FavoriteService()
---
+++ sop_django/service/service/FavoriteService.py
@@ -0,0 +1,36 @@
+from service.models import Favorite
+from service.utility.DataValidator import DataValidator
+from .BaseService import BaseService
+from django.db import connection
+
+
+class FavoriteService(BaseService):
+
+    def search(self, params):
+        print('Page No -->', params['pageNo'])
+        pageNo = (params['pageNo'] - 1) * self.pageSize
+        sql = 'select * from sos_favorite where 1=1'
+        val = params.get('name', None)
+        if (DataValidator.isNotNull(val)):
+            sql += " and name = '" + val + "' "
+        sql += " limit %s,%s"
+        cursor = connection.cursor()
+        print("------------>", sql, pageNo, self.pageSize)
+        params['index'] = ((params['pageNo'] - 1) * self.pageSize) + 1
+        cursor.execute(sql, [pageNo, self.pageSize])
+        result = cursor.fetchall()
+        columnName = ('id', 'product', 'addedDate')
+        res = {
+            "data": [],
+            "MaxId": 1,
+        }
+        count = 0
+        res["index"] = params["index"]
+        for x in result:
+            print({columnName[i]: x[i] for i, _ in enumerate(x)})
+            params['MaxId'] = x[0]
+            res['data'].append({columnName[i]: x[i] for i, _ in enumerate(x)})
+        return res
+
+    def get_model(self):
+        return Favorite
